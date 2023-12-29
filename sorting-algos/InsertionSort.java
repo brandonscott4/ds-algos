f9c4d322fe892861dda12a5204c3e26d2b5844c7
brandonscott4 <bscott15@sky.com>
2023-12-29 13:45:49 +0000

    insertion sort implementation

---
+++ sorting-algos/InsertionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    
+    public int[] sort (int[] arr){
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i; j>0; j--){
+                if(j != 0 && arr[j-1] > arr[j]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        InsertionSort is = new InsertionSort();
+        int[] arr = {4, 6, 5, 3, 2, 7, 1 ,0, 8};
+        arr = is.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
